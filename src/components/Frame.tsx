7c2bec77bd4e53fad486b695b799bffc2663b754
PlanC <hubenchang0515@outlook.com>
2024-10-20 10:56:58 +0000

    转发 iframe 的点击事件

--- src/components/Frame.tsx
+++ src/components/Frame.tsx
@@ -1,6 +1,6 @@
 import { Box, Button } from "@mui/material";
 import FabMessage, { FabMessageProps } from "./FabMessage";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { t } from "i18next";
 
 export interface FrameProps {
@@ -8,13 +8,28 @@ export interface FrameProps {
     severity?: FabMessageProps['severity'];
 }
 
+
 export default function Frame(props:FrameProps) {
     const openImmediately = () => ["warning", "error"].includes(props.severity??"");
     const [messageOpen, setMessageOpen] = useState<boolean>(openImmediately());
     useEffect(()=>setMessageOpen(openImmediately()), [props.url, props.severity]);
 
+    // 转发 iframe 的点击事件
+    // 跨域时 iframe?.contentWindow?.document 会产生 SecurityError
+    // 而 iframe?.contentDocument 会返回 undefined
+    const divRef = useRef<HTMLDivElement>(null);
+    const iframeRef = useRef<HTMLIFrameElement>(null);
+    const [iframeBody, setIframeBody] = useState<HTMLElement>();
+    useEffect(() => {
+        if (iframeBody) {
+            iframeBody.onclick = () => {
+                divRef.current?.click();
+            }
+        }
+    }, [iframeBody]);
+
     return (
-        <Box sx={{width: '100%', height: '100%'}}>
+        <Box sx={{width: '100%', height: '100%'}} ref={divRef}>
             <FabMessage 
                 open={messageOpen} 
                 title={t("common.notice")}
@@ -32,6 +47,8 @@ export default function Frame(props:FrameProps) {
             <iframe
                 src={props.url} 
                 style={{display:'block', border: 0, width: '100%', height: '100%'}}
+                ref={iframeRef}
+                onLoad={()=>setIframeBody(iframeRef.current?.contentDocument?.body)}
             />
         </Box>
     )
