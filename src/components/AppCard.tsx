3fa2ebadcff8b3232351fd47cbb3ff73936a61cd
PlanC <hubenchang0515@outlook.com>
2024-11-09 09:00:07 +0000

    增加应用卡片组件

---
+++ src/components/AppCard.tsx
@@ -0,0 +1,41 @@
+import { Box, Card, CardContent, CardMedia, IconButton, Typography } from "@mui/material";
+import LaunchIcon from '@mui/icons-material/Launch';
+
+export interface AppCardProps {
+    name: string;
+    url: string;
+    image: string;
+}
+
+export default function AppCard(props:AppCardProps) {
+    return (
+        <Card 
+            sx={{ 
+                display: 'flex', 
+                height:'100%',
+                ':hover': {
+                    boxShadow: 12,
+                }
+            }}
+        >
+            <CardMedia
+                component="img"
+                sx={{ width: '50%', imageRendering:'pixelated' }}
+                image={props.image}
+                alt="Live from space album cover"
+            />
+            <Box sx={{ flexGrow:1, display: 'flex', flexDirection: 'column' }}>
+                <CardContent sx={{ flex: '1 0 auto' }}>
+                <Typography component="div" variant="subtitle1">
+                    {props.name}
+                </Typography>
+                </CardContent>
+                <Box sx={{ display: 'flex', alignItems: 'center', justifyContent:'end', p: 1}}>
+                    <IconButton href={props.url}>
+                        <LaunchIcon/>
+                    </IconButton>
+                </Box>
+            </Box>
+        </Card>
+    )
+}
