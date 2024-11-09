ae7bf5c6b24cdd7f5a25f6630266d3c3ddf874ea
PlanC <hubenchang0515@outlook.com>
2024-11-09 09:20:05 +0000

    自动补全语言选项，初始选中 cpp

--- src/pages/CodeEditor.tsx
+++ src/pages/CodeEditor.tsx
@@ -1,23 +1,14 @@
-import { Box, Button, Container, FormControl, InputLabel, MenuItem, Select } from "@mui/material";
+import { Autocomplete, Box, Button, Container, TextField } from "@mui/material";
 import HighlightEditor, { listLanguages } from "../components/HighlightEditor";
 import { useTranslation } from "react-i18next";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 const languages = listLanguages();
 
 export default function CodeEditor() {
     const { t } = useTranslation();
     const [data, setData] = useState("");
-    const [choice, setChoice] = useState(0);
-    const [language, setLanguage] = useState("txt");
-
-    useEffect(() => {
-        if (choice < languages.length) {
-            setLanguage(languages[choice]);
-        } else {
-            setLanguage("txt");
-        }
-    }, [choice])
+    const [language, setLanguage] = useState("cpp");
 
     return (
         <Container
@@ -33,53 +24,54 @@ export default function CodeEditor() {
         >
 
             <Box display={"flex"} gap={2}>
-                <Button variant="contained" component="label">
-                    {t("common.open")}
-                    <input 
-                        type="file"
-                        style={{
-                            clip: 'rect(0 0 0 0)',
-                            clipPath: 'inset(50%)',
-                            width: 0,
-                            height: 0,
+                <Box display={"flex"} flexDirection={"column"} justifyContent={"center"}>
+                    <Button variant="contained" component="label">
+                        {t("common.open")}
+                        <input 
+                            type="file"
+                            style={{
+                                clip: 'rect(0 0 0 0)',
+                                clipPath: 'inset(50%)',
+                                width: 0,
+                                height: 0,
+                            }}
+                            onChange={async (ev) =>{
+                                if (ev.target.files && ev.target.files.length > 0) {
+                                    const file = ev.target.files[0];
+                                    setData(await file.text());
+                                    ev.target.value = "";
+                                }
+                            }}
+                        />
+                    </Button>
+                </Box>
+                
+                <Box display={"flex"} flexDirection={"column"} justifyContent={"center"}>
+                    <Button 
+                        variant="contained"
+                        onClick={()=>{
+                            const blob = new Blob([data],{type: 'text/plain'});
+                            const url = URL.createObjectURL(blob);
+                            const a = document.createElement('a');
+                            a.href = url;
+                            a.download = `file.${language}`;
+                            document.body.appendChild(a);
+                            a.click();
+                            document.body.removeChild(a);
+                            URL.revokeObjectURL(url);
                         }}
-                        onChange={async (ev) =>{
-                            if (ev.target.files && ev.target.files.length > 0) {
-                                const file = ev.target.files[0];
-                                setData(await file.text());
-                                ev.target.value = "";
-                            }
-                        }}
-                    />
-                </Button>
+                    >
+                        {t("common.save")}
+                    </Button>
+                </Box>
 
-                <Button 
-                    variant="contained"
-                    onClick={()=>{
-                        const blob = new Blob([data],{type: 'text/plain'});
-                        const url = URL.createObjectURL(blob);
-                        const a = document.createElement('a');
-                        a.href = url;
-                        a.download = `file.${language}`;
-                        document.body.appendChild(a);
-                        a.click();
-                        document.body.removeChild(a);
-                        URL.revokeObjectURL(url);
-                    }}
-                >
-                    {t("common.save")}
-                </Button>
-
-                <FormControl fullWidth size="small">
-                    <InputLabel id="demo-simple-select-label">{t("code-editor.language")}</InputLabel>
-                    <Select label={t("code-editor.language")} value={`${choice}`} onChange={(ev) => {setChoice(Number(ev.target.value));}}>
-                        {
-                            languages.map((language, index) => (
-                                <MenuItem key={index} value={index}>{language}</MenuItem>
-                            ))
-                        }
-                    </Select>
-                </FormControl>
+                <Autocomplete
+                    fullWidth
+                    options={languages}
+                    value={language}
+                    onChange={(_, value) => setLanguage(value??"plaintext")}
+                    renderInput={(params) => <TextField {...params} label={t("code-editor.language")} />}
+                />
             </Box>
             
             <HighlightEditor language={language} text={data} sx={{position:'relative', flexGrow:1, overflow:'auto'}}/>
